77201f0c16a9d369058aba1dc8b5503e397b7055
James McMahon <james1345@googlemail.com>
2015-09-11 14:59:38 +0100

    excluded build specific files from distributable

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'knox_project']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
