5c78083fa17d30fcf145e1029fc5996c1f6f1341
James McMahon <james1345@googlemail.com>
2015-09-11 16:00:53 +0100

    pritified knox admin

--- knox/admin.py
+++ knox/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from knox.models import EmailConfirmation
 
-admin.site.register(EmailConfirmation)
+@admin.register(EmailConfirmation)
+class EmailConfirmationAdmin(admin.ModelAdmin):
+    list_display = ('account', 'verified',)
