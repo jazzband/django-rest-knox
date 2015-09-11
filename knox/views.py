24e644c3a100e7a28591ed4014f2d0e13e9b635b
James McMahon <james1345@googlemail.com>
2015-09-12 00:47:45 +0100

    added own tokenAuth class

--- knox/views.py
+++ knox/views.py
@@ -1,7 +1,9 @@
-from rest_framework.authentication import TokenAuthentication, SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from knox.auth import TokenAuthentication
 
 class Login(APIView):
     authentication_classes = (BasicAuthentication,)
--- knox_project/settings.py
+++ knox_project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'knox',
 )
 
