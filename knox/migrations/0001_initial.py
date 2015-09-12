438915f59b46564af4dbfca61dace74b292fd377
James McMahon <james1345@googlemail.com>
2015-09-12 17:10:44 +0100

    migrations

---
+++ knox/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AuthToken',
+            fields=[
+                ('key', models.CharField(max_length=64, serialize=False, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='auth_token_set')),
+            ],
+        ),
+    ]
