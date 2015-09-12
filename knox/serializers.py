366093284b36fbfa378d505382ae33ceeebfff7e
James McMahon <james1345@googlemail.com>
2015-09-12 17:07:06 +0100

    removed email from serialized form

--- knox/serializers.py
+++ knox/serializers.py
@@ -7,4 +7,4 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email',)
+        fields = ('username', 'first_name', 'last_name',)
