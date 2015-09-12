a38885d9d93fbc48d6a7a7a8d84d105a6cec037b
James McMahon <james1345@googlemail.com>
2015-09-12 18:42:36 +0100

    test framework in place

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,5 @@
 import json
+import base64
 
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
@@ -8,8 +9,19 @@ from knox.models import AuthToken
 
 User = get_user_model()
 
+def get_basic_auth_header(username, password):
+    return 'Basic %s' % base64.b64encode(('%s:%s' % (username, password)).encode('ascii')).decode()
+
 class AuthTestCase(TestCase):
 
     def test_login_creates_keys(self):
-        User.objects.create_user('root', 'root@localhost.com', 'toor')
-        
+        username, password = 'root', 'toor'
+        User.objects.create_user(username, 'root@localhost.com', password)
+        url = reverse('knox_login')
+        data = {}
+        self.client.credentials(HTTP_AUTHORIZATION=get_basic_auth_header(username, password))
+
+        for _ in range(5):
+            self.client.post(url, data, format='json')
+
+        self.assertEqual(AuthToken.objects.count(), 5)
