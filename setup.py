442ea9e265055b73d81204dc037dbe0d9ea73a01
James McMahon <james1345@googlemail.com>
2015-10-13 20:25:16 +0100

    changes for release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.0',
+    version='2.1.0',
 
     description='Authentication for django rest framework',
     long_description=long_description,
