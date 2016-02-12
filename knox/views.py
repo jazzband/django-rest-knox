3f5b308093367a61796dff8a2f29f8a86cd43806
Hyungsuk Yoon <youhanmir@gmail.com>
2016-02-11 17:12:12 -0800

    Remove unnecessary import

--- knox/views.py
+++ knox/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 
 from knox.auth import TokenAuthentication
