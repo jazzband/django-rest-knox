8fde5f0a4dc6c29dfd82107c4ba02c8b644425a1
belugame <belugame@users.noreply.github.com>
2018-11-21 12:38:56 +0100

    Fix redundant whitespace

--- knox/models.py
+++ knox/models.py
@@ -37,7 +37,7 @@ class AuthToken(models.Model):
     user = models.ForeignKey(User, null=False, blank=False,
                              related_name='auth_token_set', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    expires = models. DateTimeField(null=True, blank=True)
+    expires = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return '%s : %s' % (self.digest, self.user)
