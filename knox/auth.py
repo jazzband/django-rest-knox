655c234a859248c5669b4017f951cfb07de89184
Moises Rezonzew <moisesrt@gmail.com>
2018-12-09 18:01:32 +0200

    run renew token only if the token expires

--- knox/auth.py
+++ knox/auth.py
@@ -74,7 +74,7 @@ class TokenAuthentication(BaseAuthentication):
             except (TypeError, binascii.Error):
                 raise exceptions.AuthenticationFailed(msg)
             if compare_digest(digest, auth_token.digest):
-                if knox_settings.AUTO_REFRESH:
+                if knox_settings.AUTO_REFRESH and auth_token.expires:
                     self.renew_token(auth_token)
                 return self.validate_user(auth_token)
         raise exceptions.AuthenticationFailed(msg)
