58418eec0154b6ca9d3a6d595e24ba9cbcbd1e4e
Rotzbua <Rotzbua@users.noreply.github.com>
2020-09-26 02:40:29 +0200

    add new python and django to test
    add django 3.0 3.1
    add python 3.8

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # What does your project relate to?
