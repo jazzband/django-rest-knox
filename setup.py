66e8821baa92510657f9252430af0a4ce136ce91
Rotzbua <Rotzbua@users.noreply.github.com>
2020-10-31 15:41:42 +0100

    add python 3.9, remove eol 3.5

--- setup.py
+++ setup.py
@@ -47,10 +47,10 @@ setup(
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # What does your project relate to?
