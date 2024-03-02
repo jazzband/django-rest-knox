cb79440e9cb4622c9b64fbe9e139b005c551c20f
Paul Gueltekin <paul@gueltekin.at>
2024-03-02 23:14:03 +0100

    return the token as message to the user

--- knox/admin.py
+++ knox/admin.py
@@ -1,11 +1,16 @@
 from django import forms
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.auth import get_user_model
 from knox.settings import CONSTANTS
 from knox import models
 
 
 class AuthTokenCreateForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(AuthTokenCreateForm, self).__init__(*args, **kwargs)
+        self.token = None
+
     class Meta:
         model = models.AuthToken
         fields = ['user', 'expiry']
@@ -15,6 +20,7 @@ class AuthTokenCreateForm(forms.ModelForm):
         digest, token = models.get_digest_token()
         obj.digest = digest
         obj.token_key = token[:CONSTANTS.TOKEN_KEY_LENGTH]
+        self.token = token
         if commit:
             obj.save()
             obj.save_m2m()
@@ -25,7 +31,7 @@ class AuthTokenCreateForm(forms.ModelForm):
 class AuthTokenAdmin(admin.ModelAdmin):
     add_form = AuthTokenCreateForm
     list_display = ('digest', 'user', 'created', 'expiry',)
-    search_fields = ['digest', 'user__'+get_user_model().USERNAME_FIELD]  # We dont know how a custom User model looks like, but is must have a USERNAME_FIELD
+    search_fields = ['digest', 'token_key', 'user__'+get_user_model().USERNAME_FIELD]  # We dont know how a custom User model looks like, but is must have a USERNAME_FIELD
     fields = ()
     raw_id_fields = ('user',)
 
@@ -35,3 +41,7 @@ class AuthTokenAdmin(admin.ModelAdmin):
             defaults['form'] = self.add_form
         defaults.update(kwargs)
         return super(AuthTokenAdmin, self).get_form(request, obj, **defaults)
+
+    def save_model(self, request, obj, form, change):
+        self.message_user(request, "TOKEN " + form.token, messages.INFO)
+        super(AuthTokenAdmin, self).save_model(request, obj, form, change)
