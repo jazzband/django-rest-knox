28b9b18f10e22cb9fb6a0902ca1f5e2069d7b695
Paul Gueltekin <paul@gueltekin.at>
2024-03-02 21:23:16 +0100

    add admin urls to reference project

--- knox_project/urls.py
+++ knox_project/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, re_path
+from django.contrib import admin
 
 from .views import RootView
 
 urlpatterns = [
     re_path(r'^api/', include('knox.urls')),
     re_path(r'^api/$', RootView.as_view(), name="api-root"),
+    re_path(r'^admin/', admin.site.urls),
 ]
