bc0c56ffcf50725c249de132e30c84a4f5c7043c
Paul Gueltekin <paul@gueltekin.at>
2024-03-02 21:22:04 +0100

    Add admin form for creating AuthTokens, add admin filter for searching AuthTokens

--- knox/admin.py
+++ knox/admin.py
@@ -1,10 +1,37 @@
+from django import forms
 from django.contrib import admin
-
+from django.contrib.auth import get_user_model
+from knox.settings import CONSTANTS
 from knox import models
+
+
+class AuthTokenCreateForm(forms.ModelForm):
+    class Meta:
+        model = models.AuthToken
+        fields = ['user', 'expiry']
+
+    def save(self, commit=True):
+        obj = super(AuthTokenCreateForm, self).save(commit=False)
+        digest, token = models.get_digest_token()
+        obj.digest = digest
+        obj.token_key = token[:CONSTANTS.TOKEN_KEY_LENGTH]
+        if commit:
+            obj.save()
+            obj.save_m2m()
+        return obj
 
 
 @admin.register(models.AuthToken)
 class AuthTokenAdmin(admin.ModelAdmin):
+    add_form = AuthTokenCreateForm
     list_display = ('digest', 'user', 'created', 'expiry',)
+    search_fields = ['digest', 'user__'+get_user_model().USERNAME_FIELD]  # We dont know how a custom User model looks like, but is must have a USERNAME_FIELD
     fields = ()
     raw_id_fields = ('user',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        defaults = {}
+        if obj is None:
+            defaults['form'] = self.add_form
+        defaults.update(kwargs)
+        return super(AuthTokenAdmin, self).get_form(request, obj, **defaults)
